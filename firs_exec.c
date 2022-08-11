61f2b257d71b2c06060d71d087b4867da7d5ee2e
lepreto <pretorius1724@gmail.com>
2022-08-11 06:00:27 -0700

    checking

---
+++ firs_exec.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * main - a shell program
+ * @ac: the number of arguments passed.
+ * @av: array of arguments.
+ *
+ * Return: 0 if successful, 1 on EOF, Otherwise 1.
+ */
+int main(int ac, char **av)
+{
+	ssize_t nread;
+	size_t len = 0;
+	char **argv;
+	char *line, *nline;
+	/*pid_t child_pid;*/
+	/*int i;*/
+	
+	if (ac != 1)
+	{
+		perror("Error");
+		return (1);
+	}
+	while (1)
+	{
+		putprompt();
+		nread = getline(&nline, &len, stdin);
+		if (nread == -1)
+			break;
+		line = commandline(nline, nread);
+		argv = split_string_words(line);
+		/*child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror(av[0]);
+			return (1);++
+		}*/
+		/*if (child_pid == 0)
+		{*/
+			if (_strlen(line) != 0)
+			{	
+				if (execve(argv[0], argv, NULL) == -1)
+					perror(av[0]);
+			}
+			free(line);
+			free(nline);
+			line = NULL;
+			nline = NULL;
+			/*return (1);*/
+		/*}
+		else
+		{
+			wait(NULL);
+		}*/
+	}
+	free(line);
+	free(nline);
+	return (0);
+}
